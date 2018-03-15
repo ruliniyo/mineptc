f302914f5b55ca5e2e88d409c63a764cb8f64b04
Rulo Mora <rulanxa@gmail.com>
2018-03-16 00:05:54 +0100

    -Creados panel y webBrowser. -Botones de ir, actualizar y atras en funcionamiento. -Pagina de inicio "www.minarpesetacoin.com/?refer=263"

--- MinePTC/MinePTC/Form1.Designer.cs
+++ MinePTC/MinePTC/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtbdireccion = new System.Windows.Forms.TextBox();
+            this.btnback = new System.Windows.Forms.Button();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtbdireccion);
+            this.panel1.Controls.Add(this.btnback);
+            this.panel1.Controls.Add(this.btnrefresh);
+            this.panel1.Controls.Add(this.btnsend);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(973, 97);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtbdireccion
+            // 
+            this.txtbdireccion.Location = new System.Drawing.Point(142, 40);
+            this.txtbdireccion.Name = "txtbdireccion";
+            this.txtbdireccion.Size = new System.Drawing.Size(312, 20);
+            this.txtbdireccion.TabIndex = 3;
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(3, 4);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(122, 90);
+            this.btnback.TabIndex = 2;
+            this.btnback.Text = "Atras";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Location = new System.Drawing.Point(581, 25);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(75, 49);
+            this.btnrefresh.TabIndex = 1;
+            this.btnrefresh.Text = "Actualizar";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(504, 24);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(71, 50);
+            this.btnsend.TabIndex = 0;
+            this.btnsend.Text = "Ir";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 97);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(973, 423);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(973, 520);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnrefresh;
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.TextBox txtbdireccion;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.WebBrowser webBrowser;
     }
 }
 
--- MinePTC/MinePTC/Form1.cs
+++ MinePTC/MinePTC/Form1.cs
@@ -16,5 +16,31 @@ namespace MinePTC
         {
             InitializeComponent();
         }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("www.minarpesetacoin.com/?refer=263");
+            txtbdireccion.Text = "www.minarpesetacoin.com/?refer=263";
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(txtbdireccion.Text);
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
     }
 }
